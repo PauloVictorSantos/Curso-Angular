805874f5f879b3b9cec447f94d54c212273c2fc6
paulovictorln <paulovictor494@gmail.com>
2018-04-16 16:32:56 -0300

    Módulos (ngModule)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import {MeuPrimeiroComponent} from './meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
+import {CursosModule} from './cursos/cursos.module';
 
 
 @NgModule({
@@ -12,7 +13,8 @@ import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
     AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CursosModule
   ],
   providers: [],
   bootstrap: [AppComponent]
