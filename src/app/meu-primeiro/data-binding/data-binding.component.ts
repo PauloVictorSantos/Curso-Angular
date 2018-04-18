9383c1768f34ae31f967901b10a58de244030948
paulovictorln <paulovictor494@gmail.com>
2018-04-18 12:59:49 -0300

    event binding

---
+++ src/app/meu-primeiro/data-binding/data-binding.component.ts
@@ -0,0 +1,40 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-data-binding',
+  templateUrl: './data-binding.component.html',
+  styleUrls: ['./data-binding.component.css']
+})
+
+
+export class DataBindingComponent implements  OnInit {
+
+  url: string = 'https://github.com';
+  cursoAngular:boolean =true;
+  getValor() {
+    return 1;
+  }
+  urlImagem: string= "https://loremflickr.com/cache/resized/4577_38598851432_131216a7f8_320_240_nofilter.jpg";
+  getCurtirCurso(){
+    return true;
+  }
+   botaoclicado(){
+      alert();
+   }
+  valorAtual: string = "";
+  onKeyUp( evento: KeyboardEvent) {
+    this.valorAtual = <HTMLInputElement>(evento.target).value;
+  }
+  valorSalvo:string="";
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+  isMouseOver: boolean = false;
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+  }
+
+
+
+
+}
