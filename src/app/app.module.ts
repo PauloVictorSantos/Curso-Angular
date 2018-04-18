ef7815d626b26f6b9647bfbaefd0cc30d77e62e4
paulovictorln <paulovictor494@gmail.com>
2018-04-18 11:13:08 -0300

    Property binding + Interpolation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,12 @@ import { AppComponent } from './app.component';
 import {MeuPrimeiroComponent} from './meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component';
 import {CursosModule} from './cursos/cursos.module';
+import { DataBindingComponent } from './meu-primeiro/data-binding/data-binding.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component
+    AppComponent, MeuPrimeiroComponent, MeuPrimeiro2Component, DataBindingComponent
   ],
   imports: [
     BrowserModule,
